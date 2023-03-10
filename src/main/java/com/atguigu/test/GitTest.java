805c9167d76fd558badc31ef362734d2d8ff7335
李季祥 <979698792@qq.com>
2023-03-10 11:36:02 +0800

    master 分支 测试 gitee提交

---
+++ src/main/java/com/atguigu/test/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.test;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println(111);
+        System.out.println(222);
+        System.out.println(333);
+        System.out.println("hotfix");
+        System.out.println("hotfix hotfix");
+        System.out.println("master master");
+        System.out.println("test gitee master");
+    }
+}
